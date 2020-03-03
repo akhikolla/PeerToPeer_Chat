b8d62b984935b45626dcd26d999b985cf1bddd5d
Akhila Chowdary Kolla <31878891+akhikolla@users.noreply.github.com>
2020-03-03 03:35:13 -0700

    Update ChatNode.java

--- src/ChatNode.java
+++ src/ChatNode.java
@@ -4,15 +4,18 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 import javax.json.Json;
 import javax.json.JsonObject;
+//ChatNode implementing Thread class
 public class ChatNode extends Thread {
 	private BufferedReader buf_reader;
 	public ChatNode(Socket socket) throws IOException{
 		buf_reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));	
 	}
+//Running the threads
 public void run() {
 	 boolean flag = true;
 	 while(flag) {
 		 try {
+	         //Json object used for key-value pairs
 		 JsonObject jsonobject = Json.createReader(buf_reader).readObject();
 		 if(jsonobject.containsKey("username")) {
       		 System.out.println("[" + jsonobject.getString("username") + "]: "+ jsonobject.getString("message") );
